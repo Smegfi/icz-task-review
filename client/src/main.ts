dd53f39abaaa85f341c56ac60b006ba4e34a1ff3
David Skalka <david.skalka@gmail.com>
2026-04-07 19:10:45 +0200

    Fix lint

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfigDefault } from './app/app.config';
 import { App } from './app/app';
-import { Capacitor } from '@capacitor/core';
 import { registerLocaleData } from '@angular/common';
 import localeCs from '@angular/common/locales/cs';
 
-let config = appConfigDefault;
+const config = appConfigDefault;
 
-const platform = Capacitor.getPlatform();
 
 
 registerLocaleData(localeCs);
